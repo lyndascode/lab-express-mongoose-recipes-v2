6f05bb3b51f0cd1a1f7114d83018f401a3ef1670
lynda Sadoudi <lyndaironhack@gmail.com>
2025-05-18 16:27:44 +0200

    end

--- app.js
+++ app.js
@@ -11,9 +11,17 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require('mongoose');
 
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+    .connect(MONGODB_URI)
+    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch((err) => console.error("Error connecting to mongo", err));
+
+// ...
 // ROUTES
 //  GET  / route - This is just an example route
 app.get('/', (req, res) => {
@@ -24,23 +32,51 @@ app.get('/', (req, res) => {
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
-
+app.post('/recipes', (req, res) => {
+    const newRecipe = req.body;
+    Recipe.create(newRecipe)
+        .then((recipe) => res.status(201).json({ data: recipe }))
+        .catch((err) => res.status(500).json({ message: "Error" }))
+})
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get('/recipes', (req, res) => {
+    const newRecipe = req.body;
+    Recipe.find(newRecipe)
+        .then((recipe) => res.status(200).json(recipe))
+        .catch((err) => res.status(500).json({ message: "Error" }))
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get('/recipes/:id', (req, res) => {
+    const { id } = req.params;
 
+    Recipe.findById(id)
+        .then((recipe) => res.status(200).json(recipe))
+        .catch((err) => res.status(500).json({ message: "Error" }))
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put('/recipes/:id', (req, res) => {
+    const { id } = req.params;
+    const newRecipe = req.body;
+    Recipe.findByIdAndUpdate(id, newRecipe, { new: true })
+        .then((recipe) => res.status(200).json(recipe))
+        .catch((err) => res.status(500).json({ message: "Error" }))
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
+app.delete('/recipes/:id', (req, res) => {
+    const { id } = req.params;
 
+    Recipe.findByIdAndDelete(id)
+        .then(() => res.sendStatus(404))
+        .catch((err) => res.status(500).json({ message: "Error" }))
+})
 
 // Start the server
 app.listen(3000, () => console.log('My first app listening on port 3000!'));
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,25 @@
 // Your code here ...
+const mongoose = require('mongoose');
+
+
+const recipeSchema = new mongoose.Schema(
+    {
+        title: String,
+        instructions:
+        {
+            type: String,
+            required: yes,
+        },
+        level: {
+            type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+        },
+        ingredients: { type: [String] },
+        image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
+        duration: { type: Number, min: 0 },
+        isArchived: { type: Boolean, default: false },
+        created: { type: Date, default: Date.now }
+    }
+)
+
+
+module.exports = mongoose.model("Recipe", recipeSchema);
